f42efdc4a92ce2447dc801fa7bb69629bc2109c7
Henock L <henockl@live.com>
2023-08-24 08:22:03 +0300

    Add concern

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,2 @@
 class ApplicationController < ActionController::API
-  def serialize(obj)
-    ActiveModelSerializers::SerializableResource.new(obj)
-  end
 end
--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,50 +1,9 @@
 class CustomersController < ApplicationController
-  before_action :set_customer, only: %i[show update destroy]
-
-  # GET /customers
-  def index
-    @customers = Customer.all
-    render json: {success: true, data: serialize(@customers)}
-  end
-
-  # GET /customers/1
-  def show
-    render json: {success: true, data: serialize(@customer)}
-  end
-
-  # POST /customers
-  def create
-    @customer = Customer.new(customer_params)
-
-    if @customer.save
-      render json: {success: true, data: serialize(@customer)}, status: :created
-    else
-      render json: {success: false, error: @customer.errors}, status: :unprocessable_entity
-    end
-  end
-
-  # PATCH/PUT /customers/1
-  def update
-    if @customer.update(customer_params)
-      render json: {success: true, data: serialize(@customer)}
-    else
-      render json: {success: false, error: @customer.errors}, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /customers/1
-  def destroy
-    @customer.destroy
-  end
+  include Common
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_customer
-      @customer = Customer.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def customer_params
-      params.require(:payload).permit(:code, :name)
-    end
+  def model_params
+    params.require(:payload).permit(:code, :name)
+  end
 end
